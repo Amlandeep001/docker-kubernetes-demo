f920f1aac3336c5549eca513fa1b4389e1983400
Amlan Deep Nandi <Amlandeep001@users.noreply.github.com>
2022-12-11 20:36:12 +0530

    Initial Commit
    First commit with basic docker and kubernetes commands.

---
+++ src/main/java/com/abc/playjava/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.abc.playjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController
+{
+	@GetMapping("/test-docker")
+	public String getData()
+	{
+		return "sample docker app";
+	}
+
+}
---
+++ src/test/java/com/abc/playjava/DockerDemoApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.abc.playjava;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class DockerDemoApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
