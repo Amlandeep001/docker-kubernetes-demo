7e5a2426a3b97852820995a9689d210dcf195753
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2023-10-02 21:08:51 +0530

    Clean up
    Spaces cleaned up.

--- src/main/java/com/abc/playjava/DockerDemoApplication.java
+++ src/main/java/com/abc/playjava/DockerDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerDemoApplication {
-
-	public static void main(String[] args) {
+public class DockerDemoApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DockerDemoApplication.class, args);
 	}
-
 }
--- src/main/java/com/abc/playjava/controller/MyController.java
+++ src/main/java/com/abc/playjava/controller/MyController.java
@@ -11,5 +11,4 @@ public class MyController
 	{
 		return "sample docker app";
 	}
-
 }
